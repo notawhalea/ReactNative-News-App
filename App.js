23f615e527d0f3d67a5914c5895502a97aa7e0ee
Nikita <nickpelmog2003@gmail.com>
2022-12-18 18:08:33 +0300

    feat: add non-default article viewing

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import { Post } from './components/Post';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Ivan Tripuz!</Text>
+    <View>
+      <Post title="Work" createdAt="2016-05-02" imageUrl="https://image.shutterstock.com/image-vector/uri-upper-respiratory-infection-acronym-260nw-1909354021.jpg"/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
